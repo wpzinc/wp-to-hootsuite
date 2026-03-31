884db5147c407fd3de2000efc495c1c148a34bff
Tim Carr <tim@n7studios.co.uk>
2026-03-31 20:50:14 +0800

    Coding standards

--- lib/includes/class-wp-to-social-pro-settings.php
+++ lib/includes/class-wp-to-social-pro-settings.php
@@ -157,7 +157,7 @@ class WP_To_Social_Pro_Settings {
 						$this->base->plugin->filter_name . '_settings_update_settings_db_collation_error',
 						sprintf(
 							/* translators: %1$s: Documentation URL */
-							__( 'Unable to save settings due to an invalid database collation and charset on the options table. Please refer to the <a href="%1$s" target="_blank">Documentation</a>.', 'wp-to-social-pro' ),
+							__( 'Unable to save settings due to an invalid database collation and charset on the options table. Please refer to the <a href="%1$s" target="_blank">Documentation</a>.', 'wp-to-hootsuite' ),
 							'https://www.wpzinc.com/documentation/wordpress-buffer-pro/debugging-issues/#unable-to-save-settings-due-to-an-invalid-database-collation-and-charset-on-the-options-table'
 						),
 					);
@@ -166,7 +166,7 @@ class WP_To_Social_Pro_Settings {
 				// No changes were made to the settings.
 				return new WP_Error(
 					$this->base->plugin->filter_name . '_settings_update_settings_no_changes',
-					__( 'Unable to save settings due to an error. Please try again.', 'wp-to-social-pro' )
+					__( 'Unable to save settings due to an error. Please try again.', 'wp-to-hootsuite' )
 				);
 			}
 		}
@@ -177,7 +177,7 @@ class WP_To_Social_Pro_Settings {
 			// Fetch Post Type Name, Profile Name and Action Name.
 			$post_type_object = get_post_type_object( $type );
 			if ( $duplicates['profile_id'] === 'default' ) {
-				$profile = __( 'Defaults', 'wp-to-buffer' );
+				$profile = __( 'Defaults', 'wp-to-hootsuite' );
 			} elseif ( isset( $profiles[ $profile_id ] ) ) {
 				$profile = $profiles[ $profile_id ]['formatted_service'] . ': ' . $profiles[ $profile_id ]['formatted_username'];
 			}
@@ -189,7 +189,7 @@ class WP_To_Social_Pro_Settings {
 				$this->base->plugin->filter_name . '_settings_update_settings_duplicates',
 				sprintf(
 					/* translators: %1$s: Post Type Name, Plural, %2$s: Social Media Profile Name, %3$s: Action (Publish, Update, Repost, Bulk Publish), %4$s: Social Media Service Name (Buffer, Hootsuite, SocialPilot) */
-					__( 'Two or more statuses defined in %1$s > %2$s > %3$s are the same. Please correct this to ensure each status update is unique, otherwise your status updates will NOT publish to %4$s as they will be seen as duplicates, which violate Facebook and Twitter\'s Terms of Service.', 'wp-to-buffer' ),
+					__( 'Two or more statuses defined in %1$s > %2$s > %3$s are the same. Please correct this to ensure each status update is unique, otherwise your status updates will NOT publish to %4$s as they will be seen as duplicates, which violate Facebook and Twitter\'s Terms of Service.', 'wp-to-hootsuite' ),
 					$post_type_object->label,
 					$profile,
 					$action,
