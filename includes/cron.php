b19f9040bb26fff821dc24ede11e5ec524b73162
Tim Carr <tim@n7studios.co.uk>
2026-08-19 16:28:50 +0800

    Fix: Authentication: Schedule refreshing tokens

--- includes/cron.php
+++ includes/cron.php
@@ -25,3 +25,23 @@ function wp_to_hootsuite_log_cleanup_cron() {
 
 }
 add_action( 'wp_to_hootsuite_log_cleanup_cron', 'wp_to_hootsuite_log_cleanup_cron' );
+
+/**
+ * Define the WP Cron function to refresh access tokens before they expire
+ *
+ * @since   6.2.0
+ */
+function wp_to_hootsuite_refresh_token_cron() {
+
+	// Initialise Plugin.
+	$wp_to_hootsuite = WP_To_Hootsuite::get_instance();
+	$wp_to_hootsuite->initialize();
+
+	// Refresh any access tokens that are due to expire.
+	$wp_to_hootsuite->get_class( 'cron' )->refresh_token();
+
+	// Shutdown.
+	unset( $wp_to_hootsuite );
+
+}
+add_action( 'wp_to_hootsuite_refresh_token_cron', 'wp_to_hootsuite_refresh_token_cron' );
